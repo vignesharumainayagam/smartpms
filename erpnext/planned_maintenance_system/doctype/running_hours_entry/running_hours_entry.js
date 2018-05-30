2964ba7895c51796d4400121ddf9f96183086c18
vignesharumainayagam <vigneshwaran@valiantsystems.com>
2018-05-30 12:19:30 -0700

    asdqw212as

--- erpnext/planned_maintenance_system/doctype/running_hours_entry/running_hours_entry.js
+++ erpnext/planned_maintenance_system/doctype/running_hours_entry/running_hours_entry.js
@@ -100,7 +100,7 @@ frappe.ui.form.on("Running Hours Entry", "onload", function(frm) {
                         frm.doc.table_7 = [];
                         $.each(r.message, function(i, d) {
                             var a = '';
-                            if (d.last_updated_date != null) {
+                            if (d.last_updated_date == null) {
                                 a = frappe.datetime.add_days(d.last_updated_date, 1)
                                 }
                             else{ a = '1990-01-01'}
