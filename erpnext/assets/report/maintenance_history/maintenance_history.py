02a3a2a7a209b72e66a5a9ee2471ebec1b3f4ce8
Noor Mohamed <noor@valiantsystems.com>
2018-06-11 18:51:49 +0530

    iot

--- erpnext/assets/report/maintenance_history/maintenance_history.py
+++ erpnext/assets/report/maintenance_history/maintenance_history.py
@@ -5,5 +5,28 @@ from __future__ import unicode_literals
 import frappe
 
 def execute(filters=None):
+	if not filters: filters = {}
+	conditions = get_conditions(filters)
 	columns, data = [], []
+	columns=get_columns()
 	return columns, data
+def get_columns():
+	columns = [
+		_("Equipment / Sub-Equipment Name") + "::200", 
+		_("Maintenance Type") + "::200",
+		_("Frequency") + "::120",
+		_("Frequency Type") + "::120",
+		_("Last Done Date") + "::120",
+		_("Done Running Hour") + "::200"
+		
+	]
+	return columns
+
+def get_conditions(filters):
+	conditions = ""
+	if filters.get("item"): conditions += " and M.asset_name=%(item)s"
+	if filters.get("from_date"): conditions += " and T.next_due_date >= %(from_date)s"
+	if filters.get("to_date"): conditions += " and T.next_due_date <= %(to_date)s"
+	if filters.get("classcode"): conditions += " and T.classcode=%(classcode)s"
+	if filters.get("maintenance_type"): conditions += " and T.maintenance_type=%(maintenance_type)s"
+	return conditions
